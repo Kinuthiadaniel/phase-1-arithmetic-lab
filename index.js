64789148c30908c56ea0c2f515d97c90bd8e3139
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-03-19 13:25:22 +0300

    arithmetic complete

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let multiply;
+const num1= 31;
+const num2= 2;
+multiply = num1*num2;
+console.log(multiply);
+
+let random = 10;
+let max;
+max = Math.max(2,1,18,20);
+let mod
+const num3 = 22;
+const num4 = 6
+mod = num3%num4;
+
+
+
